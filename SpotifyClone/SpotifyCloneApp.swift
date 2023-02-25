98bc439b6dbb0f5723b945b7e48c2dd8e9af4410
harundemirkaya <harundmrk@gmail.com>
2023-02-25 15:44:26 +0300

    Initial Commit

---
+++ SpotifyClone/SpotifyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCloneApp.swift
+//  SpotifyClone
+//
+//  Created by Harun Demirkaya on 25.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
